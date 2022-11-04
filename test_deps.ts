d50a848aeba61327e1c24918415dc4e6bab96aa5
GitHub <noreply@github.com>
2022-11-04 08:53:00 +0000

    Update dependencies

--- test_deps.ts
+++ test_deps.ts
@@ -2,5 +2,5 @@ export {
   assertEquals,
   assertNotEquals,
   assertThrows,
-} from "https://deno.land/std@0.161.0/testing/asserts.ts";
-export { describe, it } from "https://deno.land/std@0.161.0/testing/bdd.ts";
+} from "https://deno.land/std@0.162.0/testing/asserts.ts";
+export { describe, it } from "https://deno.land/std@0.162.0/testing/bdd.ts";
